c6575e24661ae939f10330e43e1e0cd68bfbf90f
lvlrdan1s1mo <danyamalaev1@gmail.com>
2024-12-22 18:18:26 +0100

    Add files via upload

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,24 @@
 const profile = {
     username: "Jacob",
-  playTime: 300,
-  
-changeUsername(newName) {
-    this.username = newName;
-},
-getInfo() {
-    return `${this.username} has ${this.playTime} active hours!`;
-},
+    playTime: 300,
+
+    changeUsername(newName) {
+        this.username = newName;
+        console.log(`Username successfully changed to: ${this.username}`);
+    },
+
+    updatePlayTime(newTime) {
+        if (newTime >= 0) {
+            this.playTime = newTime;
+            console.log(`PlayTime successfully updated to: ${this.playTime} hours`);
+        } else {
+            console.error("Error: PlayTime must be a non-negative number.");
+        }
+    },
+
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    }
 };
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
